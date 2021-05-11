75744444c6da14b73f2bbd6b1bb73eff3d124173
Malte Meng <mandm116@gmail.com>
2021-05-11 13:26:33 +0200

    Add current search parameters

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import time
 
 subreddits = ['CryptoCurrency', 'ethereum','investing','personalfinance','Buttcoin']
 keywords = ['Ethereum', 'ethereum','eth', 'Ether', 'ether', 'ETH']
-days = 60
+days = 180
+
 timestamp = int(time.time())
-
-data = parser.pull_submissions(subreddits, keywords, days)
-
-parser.save_to_csv('s'+'_'+timestamp+'_'+str(days)+'d', data)
+data = parser.pull_comments(subreddits, keywords, days)
+parser.save_to_csv('c'+'_'+str(timestamp)+'_'+str(days)+'d', data)
