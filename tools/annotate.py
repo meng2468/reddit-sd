0553637bfbcc568198b66c8e32ade2be5f41ecb5
Malte Meng <mandm116@gmail.com>
2021-05-11 19:11:27 +0200

    Bugfix annotate function

--- tools/annotate.py
+++ tools/annotate.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os.path
 
 def read_data(file_path):
     df = pd.read_csv(file_path)
@@ -6,28 +7,37 @@ def read_data(file_path):
 
 def annotate(file_path, output_file):
     data = pd.read_csv(file_path)
+    columns = 'id, author, created_utc, score, subreddit, text, label\n'
+    if os.path.isfile(output_file):
+        columns = ''
     with open(output_file, 'a') as file:
+        file.write(columns)
         annotation = input('Type anything to get started   ')
         annotated = 0
         while annotation != 'q':
             row = data.sample()
             print('-'*40)
-            print('Annotation', annotated)
+            print('Annotation', annotated,', Target: Ethereum')
             print('Text:', row['text'].values[0])
-            annotation = input('(f)avor, (a)gainst, (n)either, or (u)unsure? (q) to exit:  ')
-            if annotation[0] == 'f':
+            annotation = input('1. (f)avor 2. (a)gainst 3. (n)either or 4. (u)unsure? 5. (q) to exit :  ')
+            if annotation[0] in ['f', '1']:
                 annotation = 'favor'
-            elif annotation[0] == 'a':
+            elif annotation[0] in ['a','2']:
                 annotation = 'against'
-            elif annotation[0] == 'n':
+            elif annotation[0] in ['n','3']:
                 annotation = 'neither'
-            elif annotation[0] == 'u':
+            elif annotation[0] in ['u','4']:
+                print('Skipping')
                 continue
-                annotation = 'unsure'
-            elif annotation[0] == 'q':
+            elif annotation[0] in ['q', '5']:
                 print('Quitting')
-                return
-            file.write(str(row['id'].values[0])+ ', "' + row['text'].values[0] + '", ' + annotation)
+                break
+            print('Chose', annotation)
+            line =  str(row['id'].values[0]) + ', ' + str(row['author'].values[0])+ ', '
+            line += str(row['created_utc'].values[0]) + ',' + str(row['score'].values[0]) + ','
+            line += str(row['score'].values[0]) + ',' + str(row['subreddit'].values[0]) + ',"'
+            line += str(row['text'].values[0]) + '",' + annotation + '\n'
+            file.write(line)
             annotated += 1
 
 if __name__ == '__main__':
