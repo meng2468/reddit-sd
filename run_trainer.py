a447e6985a65630fd957ccb74522ff7a7990426d
Smeelock <aurelien.vungoc@gmail.com>
2021-06-01 16:11:24 +0200

    Create run_trainer

---
+++ run_trainer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding=utf-8
+""" 
+Finetuning the library models for stance detection
+--------------------------------------------------
+    Original code by HuggingFace 
+    https://github.com/huggingface/transformers/blob/master/examples/pytorch/text-classification/run_glue.py
+"""
