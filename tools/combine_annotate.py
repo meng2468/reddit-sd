bf80b3d88db3b2672253e7e16eb9dfc86e35cd58
userTogrul <togrul.abbasli1866@gmail.com>
2021-06-03 16:29:42 +0400

    Track the authors with different annotations

--- tools/combine_annotate.py
+++ tools/combine_annotate.py
@@ -12,11 +12,17 @@ def authors_selection(file_path_1, file_path_2, encoding='utf-8'):
     data_2 = read_data(file_path_2)
     #outputpath to write to the file
     output_file_path = '../data/selected_authors.csv'
+    note_same_annotation_path = '../data/not_same_annotation.csv'
     columns = 'author,label\n'
+    with open(note_same_annotation_path, 'a', encoding=encoding) as file:
+        file.write(columns)
+    
     if os.path.isfile(output_file_path):
         columns = ''
+    
     with open(output_file_path, 'a', encoding=encoding) as file:
         file.write(columns)
+        
         i = 0
         for author1, stance1 in zip(data_1['id'].values, data_1[' author'].values):
 
@@ -27,25 +33,31 @@ def authors_selection(file_path_1, file_path_2, encoding='utf-8'):
 
                 if author1 == author2:
                     author_flag = True
-                    
+
                     if stance1 == stance2:
+                        i += 1
                         final_author = author2
                         final_stance = stance2
                         line = final_author + ',' + final_stance + '\n'
                         file.write(line)
-                        break
+                    else:
+                        with open(note_same_annotation_path, 'a', encoding=encoding) as file2:
+                            line = author2 + '\n'
+                            file2.write(line)
 
                     break
 
             '''
             Un commend to add authors that are not in other file
             '''
-            # if author_flag == False:
-            #     print('Author ', author1, ' not found, adding to selection file!!!!')
-            #     final_author = author1
-            #     final_stance = stance1
-            #     line = final_author + ',' + final_stance + '\n'
-            #     file.write(line)
+            if author_flag == False:
+                print('Author ', author1, ' not found, adding to selection file!!!!')
+                final_author = author1
+                final_stance = stance1
+                line = final_author + ',' + final_stance + '\n'
+                file.write(line)
+
+        print('Count of users with same annotated stance', i)
 
 def generate_dataset(author_file_path, comments_file_path, encoding='utf-8'):
     authors_data = read_data(author_file_path)
@@ -60,25 +72,25 @@ def generate_dataset(author_file_path, comments_file_path, encoding='utf-8'):
     with open(output_file_path, 'a', encoding=encoding) as file:
         file.write(columns)
         for author, stance in zip(authors_data['author'].values, authors_data['label'].values):
-            print(author, ' ', stance)
             for sentence in comments_data[comments_data.author == author]['text'].values:
                 #uncomment this line to replace characters
                 #sentence = sentence.replace('"', '\\"')
-                line = str(author) + ',' + str(stance) + ',' + str(target) + ',`' + sentence + '`\n'
+                line = str(author) + ',' + str(stance) + ',' + str(target) + ',`' + str(sentence) + '`\n'
                 file.write(line)
 
 if __name__ == '__main__':
+    file_path_1 = '../data/auth_annotations_toghrul&malte.csv'
+    file_path_2 = '../data/auth_annotations_malte.csv'
+
     comments_file_path = '../data/p_c_1620679531_180d.csv'
     selected_authors_path = '../data/selected_authors.csv'
 
-    file_path_1 = '../data/auth_annotations_toghrul.csv'
-    file_path_2 = '../data/auth_annotations_malte.csv'
 
-    #READ DATASET WITH THIS LINE
-    df = pd.read_csv('../data/author_comment_dataset.csv', quotechar="`")
-    print(df)
 
     #UNCOMMENT to generate file with same author annotation
     #authors_selection(file_path_1, file_path_2)
     #UNCOMMENT to generate final dataset
     #generate_dataset(selected_authors_path, comments_file_path)
+    #READ DATASET WITH THIS LINE
+    df = pd.read_csv('../data/author_comment_dataset.csv', quotechar="`")
+    print(df)
